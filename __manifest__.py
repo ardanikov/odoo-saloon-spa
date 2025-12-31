f15aaad2ddcc5cd9207d7bfe0758ce091ac70e25
ardanikov <sess.uuai@gmail.com>
2025-12-31 13:07:05 +0700

    init

--- __manifest__.py
+++ __manifest__.py
@@ -19,7 +19,7 @@ Long description of module's purpose
     # always loaded
     'data': [
         'security/ir.model.access.csv',
-        'views/views.xml',
+        # 'views/views.xml',
         # 'views/templates.xml',
     ],
     # only loaded in demonstration mode
