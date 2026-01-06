aec46f243286de98d268374f2bbdd282c7b08757
arjunapriyo <arjuna_priyo_32rpl@suden.smktelkom-mlg.sch.id>
2026-01-06 13:22:53 +0700

    Update appointment model, view, and sequence

--- models/__init__.py
+++ models/__init__.py
@@ -1,15 +1,4 @@
-<<<<<<< HEAD
 # -*- coding: utf-8 -*-
-
-from . import models
-from . import pelanggan
-from . import res_partner_inherit
-from . import appointment
-from . import product_inherit
-=======
-# -*- coding: utf-8 -*-
-
-
 from . import pelanggan
 from . import res_partner_inherit
 from . import appointment
@@ -17,4 +6,3 @@ from . import product_inherit
 from . import cabang
 from . import terapis
 from . import spesialis
->>>>>>> 8658b403968cd21eecf9cac646b8f678e3001f92
